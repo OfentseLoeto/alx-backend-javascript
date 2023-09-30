41272ca779f99cfd3e376e2fda40cbe6d12e8e02
ofentse loeto <ohentseloeto610@gmail.com>
2023-09-30 08:19:43 +0000

    implementation of a class

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,5 @@
 export default class ClassRoom {
-// Implementation of a class  named classroom
   constructor(maxStudentSize) {
-    this._maxStudentSize = maxStudentSize;
+  this._maxStudentSize = maxStudentSize;
   }
 }
