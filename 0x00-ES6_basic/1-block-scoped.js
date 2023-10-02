26d19bb64db579c9d06c5b2b469ea76e4c080cc0
ofentse loeto <ohentseloeto610@gmail.com>
2023-10-02 20:42:53 +0000

    tast1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,6 @@
 export default function taskBlock(trueorFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   /*
  * modify the variables inside the function taskBlock so that
