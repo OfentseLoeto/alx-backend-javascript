04f0f30b50fac997e1a6eb4eab9ad25ab1c5f315
ofentse loeto <ohentseloeto610@gmail.com>
2023-10-02 19:28:43 +0000

    fixed eslint errors

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,18 +1,18 @@
 // Defining taskFirst as function that instantiate a variable using const
 
 export function taskFirst() {
-	const task = 'I prefer const when I can';
-	return task;
+  const task = 'I prefer const when I can';
+  return task;
 }
 
 export function getLast() {
-	return ' is ok';
+  return ' is ok';
 }
 
 // Defining taskNext as function that instantiate a variable using let
 
 export function taskNext() {
-	let combination = 'But sometimes let';
-	combination += getLast();
-	return combination;
+  let combination = 'But sometimes let';
+  combination += getLast();
+  return combination;
 }
