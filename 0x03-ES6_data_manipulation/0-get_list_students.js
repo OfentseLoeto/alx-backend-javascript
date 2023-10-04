41d060c27633570631de083bc4cb76a30a7b5d08
ofentse loeto <ohentseloeto610@gmail.com>
2023-10-04 11:11:22 +0000

    configuration files

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+export function  getListStudents() {
+
+// This function Creates a list of students with their id, names and locations
+  
+  return [
+    {id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    {id: 2, firstName: 'James' , location: 'Columbia' },
+    {id: 5, firstName: 'Sarena', location: 'San Francisco' }
+  ];
+
+}
+  const student = getListStudents();
