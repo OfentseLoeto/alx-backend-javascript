35b2c92388fb6b3a4534b48adfca993f33ec96c1
Ofentse Loeto <ohentseloeto610@gmail.com>
2023-11-21 10:09:33 +0000

    Print a message to STDOUT

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * Print the given message to STDOUT
+ *
+ * @param {string} message - Message to be displayed.
+ */
+
+function displayMessage(message){
+	console.log(message);
+}
+
+module.exports = displayMessage;
