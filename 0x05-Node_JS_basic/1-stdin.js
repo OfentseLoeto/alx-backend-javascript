34d42aaa4aac4b716398d0c518e00f9e24ca2b8f
ofentseloeto <ohentseloeto610@gmail.com>
2023-12-12 19:57:23 +0000

    fix the lint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,8 +6,8 @@ const readline = require('readline');
 
 // Create an interface for reading input from the user
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 // Display the initial message
@@ -15,8 +15,8 @@ console.log('Welcome to Hoberton School, What is your name?');
 
 // Listen to the user input
 rl.on('line', (input) => {
-	// Display the user's name
-	console.log(`Your name is: ${input}`);
-	console.log('This important software is now closing');
-	rl.close();
+  // Display the user's name
+  console.log(`Your name is: ${input}`);
+  console.log('This important software is now closing');
+  rl.close();
 });
