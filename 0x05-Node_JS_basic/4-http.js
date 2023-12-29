1d5901a64292c71bf18741c9c5a519f402c1208a
ofentse loeto <ohentseloeto@gmail.com>
2023-12-29 19:19:06 +0000

    Create a small HTTP server using Nodes HTTP module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,18 +1,11 @@
 const http = require('http');
 
-// Create an HTTP server
 const app = http.createServer((req, res) => {
-  // Set the response headers
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  // Send the response body
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!\n');
 });
 
-// Set the server to listen on port 1245
-const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+app.listen(1245);
 
 module.exports = app;
