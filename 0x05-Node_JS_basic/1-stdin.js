154816f2a29e5feb49b914ca4e5bae00603fe778
ofentse loeto <ohentseloeto@gmail.com>
2023-12-29 18:32:21 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,22 +1,17 @@
-/**
- * Writing to the stdin
- */
-
 const readline = require('readline');
 
-// Create an interface for reading input from the user
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
-// Display the initial message
-console.log('Welcome to Hoberton School, What is your name?');
+console.log('Welcome to Holberton School, what is your name?');
 
-// Listen to the user input
 rl.on('line', (input) => {
-  // Display the user's name
   console.log(`Your name is: ${input}`);
-  console.log('This important software is now closing');
   rl.close();
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
 });
