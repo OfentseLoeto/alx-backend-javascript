d648b51e25817f94bda49ef8137f7368eb474b24
ofentse loeto <ohentseloeto@gmail.com>
2023-12-29 19:32:39 +0000

    Create a more complex HTTP server using Nodes HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,27 @@
+// 5-http.js
+const http = require('http');
+const { countStudents } = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
+  } else if (req.url === '/students') {
+    countStudents('database.csv')
+      .then(() => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is the list of our students\n');
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(`Internal Server Error: ${error.message}\n`);
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
