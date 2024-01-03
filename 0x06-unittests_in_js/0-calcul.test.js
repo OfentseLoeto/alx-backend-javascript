20008d5076e0e90457b80391a82dfe6fd3e5741c
Ofentse Loeto <ohentseloeto610@gmail.com>
2024-01-03 19:11:41 +0000

    fix tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,15 +2,16 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
-  it('should round and sum two numbers', function () {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
+	it('should round and sum two numbers', function () {
+		assert.strictEqual(calculateNumber(1, 3), 4);
+		assert.strictEqual(calculateNumber(1, 3.7), 5);
+		assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+		assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+	});
 
-it('should handle rounding for the second number', () => {
-    // Add test cases for rounding the second number
-    assert.strictEqual(calculateNumber(3.2, 1.5), 5);
-  });
+	it('should handle rounding for the second number', () => {
+		// Add test cases for rounding the second number
+		assert.strictEqual(calculateNumber(3.2, 1.5), 5);
+		assert.strictEqual(calculateNumber(2.8, 1.1), 4);
+	});
 });
